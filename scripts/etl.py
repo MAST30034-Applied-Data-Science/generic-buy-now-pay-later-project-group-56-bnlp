e56593a4e5a3223b6878f8a75b2e346445b60992
911150 <87968241+911150@users.noreply.github.com>
2022-09-14 18:35:07 +1000

    Merge merchant fraud data notebook

--- scripts/etl.py
+++ scripts/etl.py
@@ -33,7 +33,6 @@ consumer = spark.read.csv("../data/tables/tbl_consumer.csv", sep = '|', header=T
 userdetails = spark.read.parquet("../data/tables/consumer_user_details.parquet")
 transaction_batch1 = spark.read.parquet("../data/tables/transactions_20210228_20210827_snapshot/")
 transaction_batch2 = spark.read.parquet("../data/tables/transactions_20210828_20220227_snapshot/")
-#population = spark.read.option("header", True).csv('../data/tables/population_data.csv')
 # transaction_batch3 = spark.read.parquet("./data/tables/<insert_folder_name>_snapshot/")
 
 """
@@ -41,32 +40,11 @@ Renaming columns, cleaning column
 and add columns accordingly
 """
 
-# External dataset 1: Estimated Region Population by SA2 Districts, 2021
-
-# Function to pre-process population data
-from pyspark.sql.types import IntegerType, LongType
-
-def population_preprocess(data):
-
-    cols_to_keep = ['sa2_maincode_2016', 'sa2_name_2016', 'erp_2021']
-    population_df = data.select(*cols_to_keep)
-
-    population_df = population_df \
-                    .withColumn("erp_2021", F.col('erp_2021').cast(IntegerType()))
-
-    population_df = population_df \
-                    .withColumnRenamed('sa2_name_2016', 'suburb') \
-                    .withColumnRenamed('erp_2021', 'estimated_region_population_2021')
-                    
-    population_df = population_df.filter(F.col('estimated_region_population_2021') > 0)
-
-    return population_df
-
-#population = population_preprocess(population)
 
 # read in processed external datasets
-population = etl_population()
+population = etl_population() # External dataset 1: Estimated Region Population by SA2 Districts, 2021
 income = etl_income()
+
 
 # Merchant data
 merchants = merchants.withColumnRenamed("name", "merchant_name")\
